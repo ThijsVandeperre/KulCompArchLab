02f1a65a86a37d520e810b44fea6282919a1695a
ThijsVandeperre <thijs.vandeperre@student.kuleuven.be>
2022-03-18 11:22:14 +0100

    Update main.c

--- app/src/main.c
+++ app/src/main.c
@@ -1,14 +1,173 @@
-/**
- * @file main.c
- * @version 0.1
- *
- */
-
 #include <stdint.h>
 #include <stm32l4xx.h>
 
-int main(void) {
+int mux = 0;
+int/*semihsemihsemih*/ tick = 0;
+int uren = 0;
+int minuten = 0;
 
-  while (1) {
-  }
+void delay(unsigned int n){
+	volatile unsigned int delay = n;
+	while (delay){
+		if (tick){
+			delay--;
+			tick = 0;
+		}
+	}
+}
+
+void clear(){
+	GPIOA->ODR &= ~(GPIO_ODR_OD7 | GPIO_ODR_OD5);
+	GPIOB->ODR &= ~(GPIO_ODR_OD0 | GPIO_ODR_OD12 | GPIO_ODR_OD15 | GPIO_ODR_OD1 | GPIO_ODR_OD2);
+}
+
+void seg7(int n){
+	switch (n){
+		case 0:
+			GPIOA->ODR |= (GPIO_/*semihsemihsemih*/ODR_OD7 | GPIO_ODR_OD5);
+			GPIOB->ODR |= (GPIO_ODR_OD0 | GPIO_ODR_OD12 | GPIO_ODR_OD15 | GPIO_ODR_OD1);
+			break;
+		case 1:
+			GPIOA->ODR |= (GPIO_ODR_OD7 | GPIO_ODR_OD5);
+			break;
+		case 2:
+			GPIOA->ODR |= (GPIO_ODR_OD7);
+			GPIOB->ODR |= (GPIO_ODR_OD0 | GPIO_ODR_OD2 | GPIO_ODR_OD15 | GPIO_ODR_OD12);
+			break;
+		case 3:
+			GPIOA->ODR |= (GPIO_ODR_OD7 | GPIO_ODR_OD5);
+			GPIOB->ODR |= (GPIO_ODR_OD0 | GPIO_ODR_OD2 | GPIO_ODR_OD12);
+			break;
+		case 4:
+			GPIOA->ODR |= (GPIO_ODR_OD7 | GPIO_ODR_OD5);
+			GPIOB->ODR |= (GPIO_ODR_OD1 | GPIO_ODR_OD2);
+			break;
+		case 5:
+			GPIOA->ODR |= (GPIO_ODR_OD5);
+			GPIOB->ODR |= (GPIO_ODR_OD0 | GPIO_ODR_OD1 | GPIO_ODR_OD2 | GPIO_ODR_OD12);
+			break;
+		case 6:
+			GPIOA->ODR |= (GPIO_ODR_OD5);
+			GPIOB->ODR |= (GPIO_ODR_OD0 | GPIO_ODR_OD1 | GPIO_ODR_OD15 | GPIO_ODR_OD12 | GPIO_ODR_OD2);
+			break;
+		case 7:
+			GPIOA->ODR |= (GPIO_ODR_OD7 | GPIO_ODR_OD5);
+			GPIOB->ODR |= (GPIO_ODR_OD0);
+			break;
+		case 8:
+			GPIOA->ODR |= (GPIO_ODR_OD7 | GPIO_ODR_OD5);
+			GPIO/*semihsemihsemih*/B->ODR |= (GPIO_ODR_OD0 | GPIO_ODR_OD12 | GPIO_ODR_OD15 | GPIO_ODR_OD1 | GPIO_ODR_OD2);
+			break;
+		case 9:
+			GPIOA->ODR |= (GPIO_ODR_OD7 | GPIO_ODR_OD5);
+			GPIOB->ODR |= (GPIO_ODR_OD0 | GPIO_ODR_OD12 | GPIO_ODR_OD1 | GPIO_ODR_OD2);
+			break;
+	}
+}
+
+void SysTick_Handler(void){
+	tick++;
+	switch(mux & 0x3){
+	case 0:
+		clear();
+		GPIOA->ODR &= ~(GPIO_ODR_OD/*semihsemihsemih*/8);
+		GPIOA->ODR &= ~(GPIO_ODR_OD15);		// 00
+		se/*semihsemihsemih*/g7(uren / 10);
+		break;
+
+	case 1:
+		if (uren >= 24){
+			clear();
+			uren = 0;
+		}
+		else{
+			clear();
+			GPIOA->ODR |= (GPIO_ODR_OD8);
+			GPIOA->ODR &= ~(GPIO_ODR_OD15);		// 10
+			seg7(uren % 10);
+		}
+		break;
+
+	case 2:		clear();
+		GPIOA->ODR &= ~(GPIO_ODR_OD8);
+		GPIOA->ODR |= (GPIO_ODR_OD15);		// 01
+		seg7((minuten / 10) % 10);
+		break;
+
+	case 3:
+		if (minuten >= 60){
+			clear();
+			minuten = 0;
+			uren ++;
+		}
+
+		else{
+			clear();
+			GPIOA->ODR |= (GPIO_ODR_OD8);
+			GPIOA->ODR |= (GPI/*semihsemihsemih*/O_ODR_OD15);		// 11
+			seg7(minuten % 10);
+		}
+		break/*semihsemihsemih*/;
+	}
+	mux++;
+}
+
+int main(void) {
+	RCC->AHB2ENR |= RCC_AHB2ENR_GPIOBEN;
+	RCC->AHB2ENR |= RCC_AHB2ENR_GPIOAEN;
+
+    //7seg leds
+	GPIOA->MODER &= ~GPIO_MODER_MODE7_Msk;
+	G/*semihsemihsemih*/PIOA->MODER |= GPIO_MODER_MODE7_0;
+	GPIOA->OTYPER &= ~GPIO_OTYPER_OT7;
+
+	GPIOA->MODER &= ~GPIO_MODER_MODE5_Msk;
+	GPIOA->MODER |= GPIO_MODER_MODE5_0;
+	GPIOA->OTYPER &= ~GPIO_OTYPER_OT5;
+
+	GPIOB->MODER &= ~GPIO_MODER_MODE0_Msk;
+	GPIOB->MODER |= GPIO_MODER_MODE0_0;
+	GPIOB->OTYPER &= ~GPIO_OTYPER_OT0;
+
+	GPIOB->MODER &= ~GPIO_MODER_MODE12_Msk;
+	GPIOB->MODER |= GPIO_MODER_MODE12_0;
+	GPIOB->OTYPER &= ~GPIO_OTYPER_OT12;
+
+	GPIOB->MODER &= ~GPIO_MODER_MODE15_Msk;
+	GPIOB->MODER |= GPIO_MODER_MODE15_0;
+	GPIOB->OTYPER &= ~GPIO_OTYPER_OT15;
+
+	GPIOB->MODER &= ~GPIO_MODER_MO/*semihsemihsemih*/DE1_Msk;
+	GPIOB->MODER |= GPIO_MODER_MODE1_0;
+	GPIOB->OTYPER &= ~GPIO_OTYPER_OT1;
+
+	GPI/*semihsemihsemih*/OB->MODER &= ~GPIO_MODER_MODE2_Msk;
+	GPIOB->MODER |= GPIO_MODER_MODE2_0;
+	GPIOB->OTYPER &= ~GPIO_OTYPER_OT2;
+
+	//mux
+	GPIOA->MODER &= ~GPIO_MOD/*semihsemihsemih*/ER_MODE8_Msk;
+	GPIOA->MODER |= GPIO_MODER_MODE8_0;
+	GPIOA->OTYPER &= ~GPIO_OTYPER_OT8;
+
+	GPIOA->MODER &= ~GPIO_MODER_MODE15_Msk;
+	GPIOA->MODER |= GPIO_MODER_MODE15_0;
+	GPIOA->OTYPER &= ~GPIO_OTYPER_OT15;
+
+	GPIOA->MODER &= ~GPIO_MODER_MODE6_Msk;
+	GPIOA->MODER |= GPIO_MODER_MODE6_0;
+	GPIOA->OTYPER &= ~GPIO_OTY/*semihsemihsemih*/PER_OT6;
+
+	// CPU Frequentie = 48 MHz
+	// Systick interrupt elke 1 ms (1kHz)  --> 48000000 Hz / 1000 Hz --> Reload = 48000
+	SysTick_Config(48000);
+
+	// Interrupt aanzetten met een prioriteit van 128
+	NVIC_SetPriority(SysTick_IRQn, 128);
+	NVIC_EnableIRQ(SysTick_IRQn);
+
+    while (1) {
+    	minuten++;
+    	de/*semihsemihsemih*/lay(1000);
+    }
 }
